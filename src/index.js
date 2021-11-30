1a4ca6c2fc949cd68119892eea4099e30e603a78
Greg Lyons <gregory.lyons512@gmail.com>
2021-11-30 15:38:58 -0600

    Connected poke_gql database. Added Query types.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
+require('dotenv').config();
+/*
+  Set up poke_gql database.
+*/
+const mysql = require('mysql2');
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 30,
+  connectTimeout: 20000,
+});
+
+db.promise().query(`SELECT * FROM generation`)
+  .then( ([results, fields]) => {
+    console.log(results);
+    console.log(fields);
+  })
+  .catch(console.log);
+
+/*
+  Set up GraphQL Server.
+*/
 const { ApolloServer } = require('apollo-server');
 
 const { schema } = require('./gql/index.js');
 
 const server = new ApolloServer({
   schema,
+  context: ({ req }) => ({
+    ...req,
+    db,
+  })
 })
 
 server
